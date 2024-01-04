9db1f2b92bb93224705a78795cc8edd992994d3a
CodeSandbox <support@codesandbox.io>
2024-01-04 09:44:17 +0000

    Sandbox snapshot commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <router-link
+      v-for="route in $router.options.routes"
+      :key="route.path"
+      :to="route.path"
+    >
+      <p class="router-link">
+        {{ route.name }}
+      </p>
+    </router-link>
+  </header>
+  <RouterView />
+</template>
+
+<style>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+body {
+  background-color: black;
+  height: 100vh;
+  align-items: center;
+}
+
+.router-link {
+  margin-left: 12px;
+  margin-right: 12px;
+  text-transform: capitalize;
+  display: inline-block;
+  font-size: 15px;
+  font-weight: 300;
+}
+</style>
---
+++ src/views/ThreeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { TresCanvas } from "@tresjs/core";
+</script>
+
+<template>
+  <TresCanvas> </TresCanvas>
+</template>
+
+<style scoped lang="scss"></style>
